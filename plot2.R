77ef703e352baf4db949e97afbfdf134c92f1c87
Kavya Sharma <kavya5454adp@gmail.com>
2025-08-25 01:02:17 +0530

    Total PM2.5 Emissions in Baltimore City

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Load data
+NEI <- readRDS("summarySCC_PM25.rds")
+
+# Subset Baltimore
+baltimore <- NEI[NEI$fips == "24510", ]
+
+# Aggregate by year
+baltimore_total <- aggregate(Emissions ~ year, baltimore, sum)
+
+# Create PNG
+png("plot2.png", width=800, height=600)
+barplot(
+  baltimore_total$Emissions,
+  names.arg = baltimore_total$year,
+  col = "orange",
+  main = "PM2.5 Emissions in Baltimore City (1999–2008)",
+  xlab = "Year",
+  ylab = "Total Emissions (tons)"
+)
+dev.off()
